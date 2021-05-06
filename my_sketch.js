8643e1f2ab564a8d5f762556aec134fd32b9dad2
Lucy Laidlaw <lucyisabellelaidlaw@gmail.com>
2021-05-06 22:04:39 +1000

    0.3 3d text attempt

--- my_sketch.js
+++ my_sketch.js
@@ -9,5 +9,9 @@ function draw() {
 
   textSize(50);
   textFont(cool_font);
+  textAlign(CENTER);
+  fill(color(2, 53, 86));
   text("Lucy Laidlaw", width/2, height/2);
+  fill(color(166, 220, 245));
+  text("Lucy Laidlaw", width/2+2, height/2+2);
 }
